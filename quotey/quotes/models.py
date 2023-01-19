6e13629c29e7f66c564d7ac0a204549a22a4984c
Ben Smith <76930048+BenSmith278@users.noreply.github.com>
2023-01-19 15:39:30 -0500

    Make Quote and Author models

--- quotey/quotes/models.py
+++ quotey/quotes/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+# A model for authors
+class Author(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+# A model for quotes
+# Quotes are linked to authors
+class Quote(models.Model):
+    quote_text = models.CharField(max_length=250)
+    pub_date = models.DateTimeField(verbose_name="date published")
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.quote_text
