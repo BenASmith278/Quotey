def37d3a944b451f7f82e6fe901437869ffdf821
Ben Smith <76930048+BenSmith278@users.noreply.github.com>
2023-01-20 18:09:22 -0500

    Add all relevant fields to models

--- quotey/quotes/models.py
+++ quotey/quotes/models.py
@@ -1,8 +1,11 @@
 from django.db import models
+import django.utils.timezone as tz
 
 # A model for authors
 class Author(models.Model):
-    name = models.CharField(max_length=50)
+    name = models.CharField(max_length=50)  # name
+    grad_year = models.IntegerField("class", default=26)  # class
+    picture = models.ImageField(default='./static/quotes/pp.png')  # picture
 
     def __str__(self):
         return self.name
@@ -10,9 +13,10 @@ class Author(models.Model):
 # A model for quotes
 # Quotes are linked to authors
 class Quote(models.Model):
-    quote_text = models.CharField(max_length=250)
-    pub_date = models.DateTimeField(verbose_name="date published")
-    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    quote_text = models.CharField(max_length=250)  # text
+    pub_date = models.DateTimeField("date published", default=tz.now)  # date published
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)  # author
+    quoted = models.DateTimeField("date quoted", null=True)  # date created
 
     def __str__(self):
         return self.quote_text
